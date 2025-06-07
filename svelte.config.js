2b045d743095b6af5b26850506f4674ac4d5c9ac
Seshal Jain <seshaljain11@gmail.com>
2025-06-07 10:09:27 +0530

    Fix base path for Github Pages

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,7 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: process.env.NODE_ENV === 'production' ? '/sveltekit-github-pages' : '',
+			base: process.env.NODE_ENV === 'production' ? '/hexawall' : '',
 		}
 	}
 };
